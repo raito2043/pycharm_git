16c2b0400681ed7629f279da1d588f67f1aa835b
raito2043 <1015881481@qq.com>
2019-07-04 22:03:39 +0800

    release 准备合并开发版 发布第一版

--- featureA.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_name():
-    print(os.path.curdir)
-
-
-def set_name():
-    pass
--- featureB.py
+++
@@ -1,7 +0,0 @@
-# coding = utf-8
-def set_name():
-    pass
-
-
-def get_name():
-    print('ok')
