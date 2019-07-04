2750b2431aba6c1d666a0ec05f257c3bc91a3d62
raito2043 <1015881481@qq.com>
2019-07-04 20:39:36 +0800

    feature1.4

--- featureA.py
+++ featureA.py
@@ -1,7 +1,7 @@
 '''
 
 =======	分隔符
-<<<<<<< HEAD 至 =======	master AAAAAAAAAAAAAAAA
-======= 至 >>>>>>> featureA	featureA 分支中该地方为内容
+至 =======	master AAAAAAAAAAAAAAAA
+======= 至分支中该地方为内容
 
 '''
--- featureB.py
+++ featureB.py
@@ -1,8 +1,8 @@
 '''
 
 =======	分隔符
-<<<<<<< HEAD 至 =======	master 分支中该地方的内容
-======= 至 >>>>>>> featurAAAAAAAAAAAAAAAAAA
+至 =======	master 分支中该地方的内容
+AAAAAAAAAAAAAAAAA
 
 
 '''
